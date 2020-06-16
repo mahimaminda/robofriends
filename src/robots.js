4d08a7f0e2552356f46b180d31ce5bdc66ea11e2
mahimaminda <mahimaminda@gmail.com>
2020-06-16 15:22:01 +0530

    this is my project

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots=[
+	{
+		id:1,
+		name:'Mahima Jain',
+		username:'captain marvel',
+		email:'mahima@gmail.com'
+	},
+	{
+		id:2,
+		name:'Rakhi Jain',
+		username:'superwomen',
+		email:'Rakhi21@gmail.com'
+	},
+	{
+		id:3,
+		name:'mohit Jain',
+		username:'ironman',
+		email:'mohit@gmail.com'
+	},
+	{
+		id:4,
+		name:'Lalit Jain',
+		username:'captain america',
+		email:'lalit@gmail.com'
+	},
+	{
+		id:5,
+		name:'Rachit minda',
+		username:'thor',
+		email:'rachu@gmail.com'
+	},
+	{
+		id:6,
+		name:'amisha minda',
+		username:'black widow',
+		email:'amu@gmail.com'
+	},
+	{
+		id:7,
+		name:'somya chaudhri',
+		username:'hulk',
+		email:'somu@gmail.com'
+	},
+	{
+		id:8,
+		name:'poojan d.',
+		username:'ant man',
+		email:'pudi@gmail.com'
+	},
+	{
+		id:9,
+		name:'shubhani kapoor',
+		username:'loki',
+		email:'shubhu@gmail.com'
+	},
+	{
+		id:10,
+		name:'vritika chandwani',
+		username:'spiderman',
+		email:'vritika@gmail.com'
+	}
+]
